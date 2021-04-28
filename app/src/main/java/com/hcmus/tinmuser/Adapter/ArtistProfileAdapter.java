d510de802ac5116812bc107f339cb285873339dd
ntan1902 <trinh.an.1902@gmail.com>
2021-04-28 21:06:19 +0700

    Update SearchFragment

---
+++ app/src/main/java/com/hcmus/tinmuser/Adapter/ArtistProfileAdapter.java
@@ -0,0 +1,89 @@
+package com.hcmus.tinmuser.Adapter;
+
+import android.content.Context;
+import android.content.Intent;
+import android.graphics.Color;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.bumptech.glide.Glide;
+import com.hcmus.tinmuser.Activity.ArtistProfileActivity;
+import com.hcmus.tinmuser.Model.Artist;
+import com.hcmus.tinmuser.R;
+
+import java.util.List;
+
+public class ArtistProfileAdapter extends RecyclerView.Adapter<ArtistProfileAdapter.ViewHolder> {
+    private Context context;
+    private List<Artist> mArtists;
+    String playType;
+    String userId;
+    public ArtistProfileAdapter(Context context, List<Artist> mArtists, String playType, String userId) {
+        this.context = context;
+        this.mArtists = mArtists;
+        this.playType = playType;
+        this.userId = userId;
+    }
+
+    @NonNull
+    @Override
+    public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View view = LayoutInflater
+                .from(parent.getContext())
+                .inflate(R.layout.artist_item,
+                        parent,
+                        false);
+        return new ArtistProfileAdapter.ViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
+        Artist artist = mArtists.get(position);
+
+        holder.artistName.setText(artist.getName());
+        holder.artistName.setTextColor(Color.parseColor("#FFFFFF"));
+
+        if (artist.getImageURL().equals("default")) {
+            holder.imageView.setImageResource(R.drawable.profile_image);
+        } else {
+            Glide.with(context)
+                    .load(artist.getImageURL())
+                    .into(holder.imageView);
+        }
+
+        holder.itemView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intentArtist = new Intent(context, ArtistProfileActivity.class);
+                intentArtist.putExtra("artistName", artist.getName());
+                intentArtist.putExtra("artistImageURL", artist.getImageURL());
+                intentArtist.putExtra("playType", playType);
+                intentArtist.putExtra("userId", userId);
+                context.startActivity(intentArtist);
+            }
+        });
+    }
+
+    @Override
+    public int getItemCount() {
+        return mArtists.size();
+    }
+
+    public class ViewHolder extends RecyclerView.ViewHolder {
+        ImageView imageView;
+        TextView artistName;
+
+
+        public ViewHolder(@NonNull View itemView) {
+            super(itemView);
+            imageView = itemView.findViewById(R.id.imageView);
+            artistName = itemView.findViewById(R.id.artistName);
+        }
+    }
+}
--- app/src/main/java/com/hcmus/tinmuser/Fragment/SearchFragment.java
+++ app/src/main/java/com/hcmus/tinmuser/Fragment/SearchFragment.java
@@ -4,6 +4,7 @@ import android.os.Bundle;
 
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.GridLayoutManager;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
@@ -21,8 +22,10 @@ import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
+import com.hcmus.tinmuser.Adapter.CategoryAdapter;
 import com.hcmus.tinmuser.Adapter.MusicAdapter;
 import com.hcmus.tinmuser.Model.Artist;
+import com.hcmus.tinmuser.Model.Category;
 import com.hcmus.tinmuser.Model.Song;
 import com.hcmus.tinmuser.Model.Music;
 import com.hcmus.tinmuser.R;
@@ -33,8 +36,12 @@ import java.util.List;
 public class SearchFragment extends Fragment {
     RecyclerView recyclerView;
     MusicAdapter musicAdapter;
+    CategoryAdapter categoryAdapter;
     EditText searchText;
-    public static List<Music> mMusics;
+
+    List<Music> mMusics;
+    List<Music> searchMusic;
+    List<Category> mCategories;
 
     FirebaseUser mUser;
     ArrayList<String> mUserListFavorites;
@@ -51,16 +58,22 @@ public class SearchFragment extends Fragment {
         // Inflate the layout for this fragment
         View view = inflater.inflate(R.layout.fragment_search, container, false);
 
-        recyclerView = view.findViewById(R.id.recyclerViewSearch);
-        recyclerView.setHasFixedSize(true);
-        recyclerView.setLayoutManager(new LinearLayoutManager(getContext()));
+
         searchText = view.findViewById(R.id.searchText);
 
+        recyclerView = view.findViewById(R.id.recyclerViewSearch);
+        recyclerView.setHasFixedSize(true);
+
+        mCategories = new ArrayList<>();
+        categoryAdapter = new CategoryAdapter(getContext(), mCategories);
+        setCategoryView();
+        getCategories();
+
         mMusics = new ArrayList<>();
+        searchMusic = new ArrayList<>();
+//        musicAdapter = new MusicAdapter(getContext(), searchMusic, "Single", "", mUserListFavorites);
+
         mUserListFavorites = new ArrayList<>();
-        setListView(mMusics);
-
-
         getMusics();
 
 
@@ -76,17 +89,19 @@ public class SearchFragment extends Fragment {
                                       int before, int count) {
                 if (searchText.hasFocus()) {
                     if (s.toString().isEmpty()) {
-                        setListView(mMusics);
-                        getMusics();
+                        setCategoryView();
+                        getCategories();
                     } else {
-                        List<Music> searchMusic = new ArrayList<>();
+                        searchMusic = new ArrayList<>();
                         for (Music x : mMusics) {
                             if (x.getArtist().getName().toLowerCase().contains(s.toString().toLowerCase()) ||
                                     x.getSong().getName().toLowerCase().contains(s.toString().toLowerCase())) {
                                 searchMusic.add(x);
+//                                musicAdapter.notifyDataSetChanged();
                             }
                         }
-                        setListView(searchMusic);
+                        setMusicView(searchMusic);
+
                     }
                 }
             }
@@ -113,10 +128,44 @@ public class SearchFragment extends Fragment {
         return view;
     }
 
-    void setListView(List<Music> list) {
+    private void setMusicView(List<Music> list) {
         musicAdapter = new MusicAdapter(getContext(), list, "Single", "", mUserListFavorites);
+        recyclerView.setLayoutManager(new LinearLayoutManager(getContext()));
         recyclerView.setAdapter(musicAdapter);
     }
+
+    private void setCategoryView() {
+        recyclerView.setLayoutManager(new GridLayoutManager(getContext(), 2));
+        recyclerView.setAdapter(categoryAdapter);
+    }
+
+
+    private void getCategories() {
+        FirebaseDatabase.getInstance().getReference("Categories")
+                .addValueEventListener(new ValueEventListener() {
+                    @Override
+                    public void onDataChange(@NonNull DataSnapshot snapshot) {
+                        mCategories.clear();
+                        for (DataSnapshot categorySnapshot : snapshot.getChildren()) {
+                            Category category = categorySnapshot.getValue(Category.class);
+
+                            mCategories.add(category);
+                            categoryAdapter.notifyDataSetChanged();
+                        }
+                    }
+
+                    @Override
+                    public void onCancelled(@NonNull DatabaseError error) {
+
+                    }
+                });
+    }
+
+//    void setListView(List<Category> list) {
+////        musicAdapter = new MusicAdapter(getContext(), list, "Single", "", mUserListFavorites);
+//        categoryAdapter = new CategoryAdapter(getContext(), list);
+//        recyclerView.setAdapter(categoryAdapter);
+//    }
 
     private void getMusics() {
         // Lấy list song
@@ -139,7 +188,7 @@ public class SearchFragment extends Fragment {
                             Artist artist = snapshot2.getValue(Artist.class);
                             Music music = new Music(song, artist);
                             mMusics.add(music);
-                            musicAdapter.notifyDataSetChanged();
+//                            musicAdapter.notifyDataSetChanged();
                         }
 
                         @Override
