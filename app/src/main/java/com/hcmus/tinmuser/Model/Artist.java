dfda5d1a2e9344d60d27b717ea078ce7c2089ec1
tloc00 <nguyen.ntl.sun04132000@gmail.com>
2021-05-21 11:08:51 +0700

    fix bug artist

--- app/src/main/java/com/hcmus/tinmuser/Model/Artist.java
+++ app/src/main/java/com/hcmus/tinmuser/Model/Artist.java
@@ -1,49 +1,49 @@
-package com.hcmus.tinmuser.Model;
-
-public class Artist {
-    String id;
-    String name;
-    String imageURL;
-    Integer totalFollow;
-
-    public Artist() {
-
-    }
-    public Artist(String id, String name, String imageURL, Integer totalFollow) {
-        this.id = id;
-        this.name = name;
-        this.imageURL = imageURL;
-        this.totalFollow = totalFollow;
-    }
-    public String getId() {
-        return id;
-    }
-
-    public void setId(String id) {
-        this.id = id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public String getImageURL() {
-        return imageURL;
-    }
-
-    public void setImageURL(String imageURL) {
-        this.imageURL = imageURL;
-    }
-
-    public Integer getTotalFollow() {
-        return totalFollow;
-    }
-
-    public void setTotalFollow(Integer totalFollow) {
-        this.totalFollow = totalFollow;
-    }
-}
+//package com.hcmus.tinmuser.Model;
+//
+//public class Artist {
+//    String id;
+//    String name;
+//    String imageURL;
+//    Integer totalFollow;
+//
+//    public Artist() {
+//
+//    }
+//    public Artist(String id, String name, String imageURL, Integer totalFollow) {
+//        this.id = id;
+//        this.name = name;
+//        this.imageURL = imageURL;
+//        this.totalFollow = totalFollow;
+//    }
+//    public String getId() {
+//        return id;
+//    }
+//
+//    public void setId(String id) {
+//        this.id = id;
+//    }
+//
+//    public String getName() {
+//        return name;
+//    }
+//
+//    public void setName(String name) {
+//        this.name = name;
+//    }
+//
+//    public String getImageURL() {
+//        return imageURL;
+//    }
+//
+//    public void setImageURL(String imageURL) {
+//        this.imageURL = imageURL;
+//    }
+//
+//    public Integer getTotalFollow() {
+//        return totalFollow;
+//    }
+//
+//    public void setTotalFollow(Integer totalFollow) {
+//        this.totalFollow = totalFollow;
+//    }
+//}
